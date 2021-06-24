2d377a9b9531c221c15ccfa3916941e8b2719934
rafius97 <raerocor97@gmail.com>
2021-06-24 07:04:47 -0400

    style: run prettier

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@
  */
 require("dotenv").config();
 require("@nomiclabs/hardhat-truffle5");
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
 require("hardhat-gas-reporter");
 
 if (!process.env.ALCHEMY_MAIN_API_KEY)
@@ -14,13 +14,13 @@ module.exports = {
     hardhat: {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAIN_API_KEY}`,
-        blockNumber: 12686302
-      }
+        blockNumber: 12686302,
+      },
     },
   },
   gasReporter: {
-    enabled: (process.env.REPORT_GAS) ? true : false,
-    currency: 'USD',
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   solidity: {
